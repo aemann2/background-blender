95f038eb50856aff23c0b5929b3c4c9543da7e00
Adam Mann <aemann2@gmail.com>
2021-02-24 15:54:47 -0800

    added JS components

--- js/main.js
+++ js/main.js
@@ -1,17 +1,51 @@
-const image = document.querySelector('#image');
-const button = document.querySelector('#new-pic');
-const url = 'https://picsum.photos/800/600?grayscale';
+const image = document.querySelector('.img-container');
+const bwButton = document.querySelector('#new-BW-pic');
+const colorButton = document.querySelector('#new-color-pic');
+const bwUrl = 'https://picsum.photos/800/600?grayscale';
+const colorUrl = 'https://picsum.photos/800/600';
 
-// funciton to fetch photos from the endpoint
-const fetchPhoto = () => {
-  fetch(url)
+const colorPicker = document.querySelector('#colorpicker');
+const root = document.documentElement;
+const blendMode = document.querySelector('#blend-mode');
+
+// function to fetch photos from the endpoint
+const fetchBWPhoto = () => {
+  fetch(bwUrl)
     .then((response) => response.blob())
     .then((data) => {
       const imgURL = URL.createObjectURL(data);
-      image.src = imgURL;
+      image.style.backgroundImage = `url('${imgURL}')`;
     });
 };
 
-fetchPhoto();
+const fetchColorPhoto = () => {
+  fetch(colorUrl)
+    .then((response) => response.blob())
+    .then((data) => {
+      const imgURL = URL.createObjectURL(data);
+      image.style.backgroundImage = `url('${imgURL}')`;
+    });
+};
 
-button.addEventListener('click', fetchPhoto);
+// resets root to transparent
+const resetRoot = () => {
+  root.style.setProperty('--color', 'transparent');
+};
+
+fetchBWPhoto();
+
+bwButton.addEventListener('click', () => {
+  fetchBWPhoto();
+  resetRoot();
+});
+
+colorButton.addEventListener('click', () => {
+  fetchColorPhoto();
+  resetRoot();
+});
+
+colorPicker.oninput = () =>
+  root.style.setProperty('--color', colorPicker.value);
+
+blendMode.oninput = () =>
+  root.style.setProperty('--blend-mode', blendMode.value);
