d302924391b5deec4baf1591aaf91aeec74e046b
Adam Mann <aemann2@gmail.com>
2021-02-26 20:39:12 -0800

    added photo button

--- js/main.js
+++ js/main.js
@@ -1,20 +1,15 @@
 const image = document.querySelector('.img__container');
 const spinner = document.querySelector('.img__spinner');
 const bwButton = document.querySelector('#new-BW-pic');
-const colorButton = document.querySelector('#new-color-pic');
 const bwUrl = 'https://picsum.photos/800?grayscale';
-const colorUrl = 'https://picsum.photos/800/600';
 
 const colorPicker = document.querySelector('#color-picker');
 const root = document.documentElement;
 const blendMode = document.querySelector('#blend-mode');
 
-console.log(spinner);
-
 // function to fetch photos from the endpoint
 const fetchBWPhoto = () => {
-  spinner.src = './images/spinner.svg';
-  image.style.backgroundImage = `url('')`;
+  image.style.backgroundImage = `url('./images/spinner.svg')`;
   fetch(bwUrl)
     .then((response) => response.blob())
     .then((data) => {
@@ -44,11 +39,6 @@ bwButton.addEventListener('click', () => {
   fetchBWPhoto();
   resetRoot();
 });
-
-// colorButton.addEventListener('click', () => {
-//   fetchColorPhoto();
-//   resetRoot();
-// });
 
 colorPicker.oninput = () =>
   root.style.setProperty('--color', colorPicker.value);
